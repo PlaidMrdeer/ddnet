af7f5cf12c00b89dfd41bfaafb04399888fb4229
plaidmrdeer <plaidmrdeer@proton.me>
2026-06-28 16:40:32 +0800

    1

--- src/game/client/my.h
+++ src/game/client/my.h
@@ -2,22 +2,37 @@
 #define GAME_CLIENT_COMPONENTS_MY_H
 
 #include <game/client/component.h>
+#include <engine/client/enums.h>
+#include <base/vmath.h>
 
 class CMyComponent : public CComponent
 {
+public:
+	enum EAimState
+	{
+		STATE_IDLE = 0,
+		STATE_AIMING_IN,
+		STATE_HOOKING,
+		STATE_AIMING_OUT
+	};
+
 private:
-	int m_TargetId = -1;
-	bool m_SilentAimActive = false;
-	vec2 m_SilentAimVector;
+	int m_aTargetId[NUM_DUMMIES];
+	EAimState m_aAimState[NUM_DUMMIES];
+	vec2 m_aCurrentAim[NUM_DUMMIES];
+	bool m_aHookOverride[NUM_DUMMIES];
+
+	float GetJitterAngle(int DummyIdx) const;
 
 public:
 	int Sizeof() const override { return sizeof(*this); }
-	void OnReset() override { m_TargetId = -1; m_SilentAimActive = false; }
+	void OnReset() override;
 	void OnUpdate() override;
 	void OnRender() override;
 
-	bool IsSilentAimActive() const { return m_SilentAimActive; }
-	vec2 GetSilentAimVector() const { return m_SilentAimVector; }
+	bool IsSilentAimActive(int DummyIdx) const;
+	vec2 GetSilentAimVector(int DummyIdx) const;
+	bool GetHookOverride(int DummyIdx) const;
 };
 
 #endif
