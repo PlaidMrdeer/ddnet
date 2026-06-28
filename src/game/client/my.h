75fd9c702dba20cdb620e0bc1913b8bea9efdf34
plaidmrdeer <plaidmrdeer@proton.me>
2026-06-28 16:14:03 +0800

    1

--- src/game/client/my.h
+++ src/game/client/my.h
@@ -7,12 +7,17 @@ class CMyComponent : public CComponent
 {
 private:
 	int m_TargetId = -1;
+	bool m_SilentAimActive = false;
+	vec2 m_SilentAimVector;
 
 public:
 	int Sizeof() const override { return sizeof(*this); }
-	void OnReset() override { m_TargetId = -1; }
+	void OnReset() override { m_TargetId = -1; m_SilentAimActive = false; }
 	void OnUpdate() override;
 	void OnRender() override;
+
+	bool IsSilentAimActive() const { return m_SilentAimActive; }
+	vec2 GetSilentAimVector() const { return m_SilentAimVector; }
 };
 
 #endif
